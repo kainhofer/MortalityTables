3098b6cc558f71a1df6e57503a7a87f54110bac8
Reinhold Kainhofer <reinhold@kainhofer.com>
2016-08-27 22:33:34 +0200

    Add US annuity tables (or rather, move them to the new structure of loading)

---
+++ inst/extdata/ValuationTables_USA_Annuities_1971IAM.R
@@ -0,0 +1,44 @@
+stopifnot(require(methods), require(utils), require(ValuationTables))
+
+
+###############################################################################
+### 1971 IAM individual annuity table; with optional projection scale B_x
+###############################################################################
+
+USA1971IAM.data = utils::read.csv(
+    system.file("extdata",
+                "USA_Annuities_1971IAM.csv",
+                package = "ValuationTables"),
+    col.names = c("age","qx", "qy", "B"),
+    skip = 3)
+
+USA1971IAM.male = valuationTable.period (
+  name = "USA 1971 IAM, male",
+  ages = USA1971IAM.data$age,
+  deathProbs = USA1971IAM.data$qx)
+
+USA1971IAM.female = valuationTable.period (
+  name = "USA 1971 IAM, female",
+  ages = USA1971IAM.data$age,
+  deathProbs = USA1971IAM.data$qy)
+
+USA1971IAM.male.projected = valuationTable.improvementFactors (
+  name = "USA 1971 IAM, male, projected",
+  ages = USA1971IAM.data$age,
+  baseYear = 1971,
+  deathProbs = USA1971IAM.data$qx,
+  improvement = USA1971IAM.data$B)
+
+USA1971IAM.female.projected = valuationTable.improvementFactors (
+  name = "USA 1971 IAM, female, projected",
+  ages = USA1971IAM.data$age,
+  baseYear = 1971,
+  deathProbs = USA1971IAM.data$qy,
+  improvement = USA1971IAM.data$B)
+
+rm(USA1971IAM.data)
+
+# plotValuationTables(USA1971IAM.male, USA1971IAM.male.projected, YOB = 1971)
+# plotValuationTables(USA1971IAM.male, USA1971IAM.male.projected, Period = 1999)
+
+
