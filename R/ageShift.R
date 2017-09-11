d8adcdc72ffc285811bef08652d80c39cbc60095
Reinhold Kainhofer <reinhold@kainhofer.com>
2017-09-11 00:21:52 +0000

    ageShift: Return NA for mortalityTables without age shift

--- R/ageShift.R
+++ R/ageShift.R
@@ -12,9 +12,17 @@ NULL
 #' ageShift(AVOe2005R.male.av, YOB=1910)
 #' ageShift(AVOe2005R.male.av, YOB=1955)
 #' ageShift(AVOe2005R.male.av, YOB=2010)
+#' # A table with trend does NOT have any age shift, so NA is returned:
+#' ageShift(AVOe2005R.male, YOB=1910)
 #'
 #' @exportMethod ageShift
 setGeneric("ageShift", function(object, YOB=1975, ...) standardGeneric("ageShift"));
+
+#' @describeIn ageShift Age shifts apply only to mortalityTagle.ageShift, so
+#'             all other tables return NA.
+setMethod("ageShift", "mortalityTable", function(object, YOB, ...) {
+    NA
+})
 
 #' @describeIn ageShift Return the age shift of the age-shifted life table
 #'                      given the birth year
